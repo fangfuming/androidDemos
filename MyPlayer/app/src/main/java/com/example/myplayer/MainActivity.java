d07529b010c2cc0f19cf45c12e06eda9a27a6fb8
方先生爱吃方便面 <1317085817@qq.com>
2020-08-01 19:47:42 +0800

    myPlayer全部完成(使用mvvm模式

--- MyPlayer/app/src/main/java/com/example/myplayer/MainActivity.java
+++ MyPlayer/app/src/main/java/com/example/myplayer/MainActivity.java
@@ -14,24 +14,33 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 import com.example.myplayer.viewModel.MediaPlayerViewModel;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
+
+
+
     private MediaPlayerViewModel mViewModel;
-
     private ProgressBar mProgressBar;
-
     private SurfaceView mSurfaceView;
     private FrameLayout playerFrame;
+    private FrameLayout controllerFrame;
+    private SeekBar mSeekBar;
+    private ImageView mPlayStatusImage;
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             hideSystemUi();
+            mViewModel.emmitVideoResolution();
         }
     }
 
@@ -39,26 +48,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initEvent();
+    }
 
-        mProgressBar = findViewById(R.id.progressBar);
-        mSurfaceView = findViewById(R.id.surfaceView);
-        playerFrame = findViewById(R.id.playerFrame);
+    private void initEvent() {
 
-
-        mViewModel = new ViewModelProvider(this).get(MediaPlayerViewModel.class);
-//        Uri mUri = Uri.parse("android.resource://" + getPackageName() + "/"+ R.raw.video2);
-//        mViewModel.initPlayer(this, mUri);
-//        mViewModel.init();
+        /**
+         * 缓存过程中的转圈圈是否可见
+         */
         mViewModel.getProgressBarVisible().observe(this, new Observer<Integer>() {
             @Override
             public void onChanged(Integer integer) {
                 mProgressBar.setVisibility(integer);
             }
         });
+        /**
+         * 观察下方控制view的可见与否
+         */
+        mViewModel.getControllerViewVisible().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                controllerFrame.setVisibility(integer);
+            }
+        });
 
         mViewModel.getVideoResolution().observe(this, new Observer<Point>() {
             @Override
             public void onChanged(final Point point) {
+                //视频分辨率出来后 时长也有了 给seekBar赋值
+                mSeekBar.setMax(mViewModel.getMediaPlayer().getDuration());
                 playerFrame.post(new Runnable() {
                     @Override
                     public void run() {
@@ -85,6 +104,76 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {}
         });
+
+        playerFrame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.toggleControllerVisibility();
+            }
+        });
+
+        mViewModel.getBufferPercent().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                mSeekBar.setSecondaryProgress(mSeekBar.getMax() * integer /100);
+            }
+        });
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //用户的拖动
+                if (fromUser) {
+                    mViewModel.seekToPosition(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mViewModel.getPlayerStatus().observe(this, new Observer<MediaPlayerViewModel.PlayerStatus>() {
+            @Override
+            public void onChanged(MediaPlayerViewModel.PlayerStatus playerStatus) {
+                mPlayStatusImage.setClickable(true);
+                if (playerStatus == MediaPlayerViewModel.PlayerStatus.NotReady) {
+                    mPlayStatusImage.setClickable(false);
+                }else if(playerStatus == MediaPlayerViewModel.PlayerStatus.Playing){
+                    mPlayStatusImage.setImageResource(R.drawable.ic_pause_black_24dp);
+                }else if(playerStatus == MediaPlayerViewModel.PlayerStatus.Pause){
+                    mPlayStatusImage.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+                }else if(playerStatus == MediaPlayerViewModel.PlayerStatus.Completed){
+                    mPlayStatusImage.setImageResource(R.drawable.ic_replay_black_24dp);
+                }
+            }
+        });
+
+        mPlayStatusImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.togglePlayerStatus();
+            }
+        });
+    }
+
+    private void initView() {
+        mProgressBar = findViewById(R.id.progressBar);
+        mSurfaceView = findViewById(R.id.surfaceView);
+        playerFrame = findViewById(R.id.playerFrame);
+        mSeekBar = findViewById(R.id.seekBar);
+        mPlayStatusImage = findViewById(R.id.playImage);
+        controllerFrame = findViewById(R.id.controllerFrame);
+        mViewModel = new ViewModelProvider(this).get(MediaPlayerViewModel.class);
+        mViewModel.setMainActivity(this);
+
+        updatePlayerProgress();
     }
 
     private void resizePlayer(int width,int height){
@@ -98,10 +187,31 @@ public class MainActivity extends AppCompatActivity {
         mSurfaceView.setLayoutParams(layoutParams);
     }
 
+    private void updatePlayerProgress(){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true){
+                        try {
+                            Thread.sleep(500);
+                            mSeekBar.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mSeekBar.setProgress(mViewModel.getMediaPlayer().getCurrentPosition());
+                                }
+                            });
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+    }
+
 
     private void hideSystemUi(){
         View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
--- MyPlayer/app/src/main/java/com/example/myplayer/viewModel/MediaPlayerViewModel.java
+++ MyPlayer/app/src/main/java/com/example/myplayer/viewModel/MediaPlayerViewModel.java
@@ -5,13 +5,17 @@ import android.graphics.Point;
 import android.media.MediaPlayer;
 import android.view.View;
 
+import androidx.annotation.UiThread;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.myplayer.MainActivity;
 import com.example.myplayer.R;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by fangfuming
@@ -19,8 +23,37 @@ import java.io.IOException;
  */
     public class MediaPlayerViewModel extends ViewModel {
 
-    private MutableLiveData<Integer> progressBarVisible = new MutableLiveData<>();
+    public enum PlayerStatus{
+        Playing,
+        Pause,
+        Completed,
+        NotReady
+    }
+
+    private MainActivity mMainActivity;
+
+    public void setMainActivity(MainActivity mainActivity) {
+        mMainActivity = mainActivity;
+    }
+
+    private MutableLiveData<Integer> progressBarVisible = new MutableLiveData<>(View.INVISIBLE); //转圈圈可见
+    private MutableLiveData<Integer> bufferPercent = new MutableLiveData<>(0); //缓冲的百分比
+    private MutableLiveData<Integer> controllerViewVisible = new MutableLiveData<>(View.VISIBLE); //下方控制条可见
     private MutableLiveData<Point> videoResolution = new MutableLiveData<>();
+    private MutableLiveData<PlayerStatus> playerStatus = new MutableLiveData<>(PlayerStatus.NotReady);
+
+
+    public void setProgressBarVisible(MutableLiveData<Integer> progressBarVisible) {
+        this.progressBarVisible = progressBarVisible;
+    }
+
+    public MutableLiveData<Integer> getControllerViewVisible() {
+        return controllerViewVisible;
+    }
+
+    public void setControllerViewVisible(MutableLiveData<Integer> controllerViewVisible) {
+        this.controllerViewVisible = controllerViewVisible;
+    }
 
     private MyMediaPlayer mMediaPlayer = new MyMediaPlayer();
 
@@ -32,6 +65,15 @@ import java.io.IOException;
         return videoResolution;
     }
 
+
+    public MutableLiveData<PlayerStatus> getPlayerStatus() {
+        return playerStatus;
+    }
+
+    public void setPlayerStatus(MutableLiveData<PlayerStatus> playerStatus) {
+        this.playerStatus = playerStatus;
+    }
+
     public MyMediaPlayer getMediaPlayer() {
         return mMediaPlayer;
     }
@@ -39,12 +81,15 @@ import java.io.IOException;
     public MediaPlayerViewModel(){
         mMediaPlayer.reset();
         videoResolution.setValue(new Point(0,0));
+        playerStatus.setValue(PlayerStatus.NotReady);
+        controllerViewVisible.setValue(View.INVISIBLE);
+        progressBarVisible.setValue(View.VISIBLE);
         try {
-            progressBarVisible.setValue(View.VISIBLE);
             mMediaPlayer.setDataSource("https://img.tukuppt.com/video_show/3670116/00/02/03/5b4f40c2b2fa3.mp4");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
 
 
         /**
@@ -54,8 +99,26 @@ import java.io.IOException;
             @Override
             public void onPrepared(MediaPlayer mp) {
                 progressBarVisible.setValue(View.INVISIBLE);
-                mMediaPlayer.setLooping(true);
                 mMediaPlayer.start();
+                playerStatus.setValue(PlayerStatus.Playing);
+            }
+        });
+
+        mMediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                 bufferPercent.setValue(percent);
+            }
+        });
+
+        /**
+         * 拖动完成监听
+         */
+        mMediaPlayer.setOnSeekCompleteListener(new MediaPlayer.OnSeekCompleteListener() {
+            @Override
+            public void onSeekComplete(MediaPlayer mp) {
+                mMediaPlayer.start();
+                progressBarVisible.setValue(View.INVISIBLE);
             }
         });
 
@@ -69,9 +132,76 @@ import java.io.IOException;
             }
         });
 
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                playerStatus.setValue(PlayerStatus.Completed);
+            }
+        });
+
+
 
         mMediaPlayer.prepareAsync();
 
+    }
+
+    public MutableLiveData<Integer> getBufferPercent() {
+        return bufferPercent;
+    }
+
+    public void setBufferPercent(MutableLiveData<Integer> bufferPercent) {
+        this.bufferPercent = bufferPercent;
+    }
+
+    //重新布局player
+    public void  emmitVideoResolution(){
+        videoResolution.setValue(videoResolution.getValue());
+    }
+
+
+    private long controllerViewShowTime = 0;
+
+    //
+    public void toggleControllerVisibility(){
+        if (View.INVISIBLE == controllerViewVisible.getValue()) {
+            controllerViewVisible.setValue(View.VISIBLE);
+            controllerViewShowTime = System.currentTimeMillis();
+            final Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (System.currentTimeMillis() - controllerViewShowTime>6000) {
+                        mMainActivity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                controllerViewVisible.setValue(View.INVISIBLE);
+                            }
+                        });
+                    }
+                }
+            },6000);
+        }else {
+            controllerViewVisible.setValue(View.INVISIBLE);
+        }
+    }
+
+    public void seekToPosition(int position){
+        progressBarVisible.setValue(View.VISIBLE);
+        mMediaPlayer.seekTo(position);
+    }
+
+
+    public void togglePlayerStatus(){
+        if (playerStatus.getValue() == PlayerStatus.Playing) {
+            mMediaPlayer.pause();
+            playerStatus.setValue(PlayerStatus.Pause);
+        }else if (playerStatus.getValue() == PlayerStatus.Pause) {
+            mMediaPlayer.start();
+            playerStatus.setValue(PlayerStatus.Playing);
+        }else if (playerStatus.getValue() == PlayerStatus.Completed) {
+            mMediaPlayer.start();
+            playerStatus.setValue(PlayerStatus.Playing);
+        }
     }
 
     @Override
